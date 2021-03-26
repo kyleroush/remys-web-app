bba0a0d2b36f963ac390795d2cc566f7c1db15b6
kr043029 <kyle.roush@cerner.com>
2021-03-26 10:49:55 -0500

    button and counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import './App.css';
 import { db } from './Firestore';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   const feedMeNow = () => {
-    db.ref('feednow').push('please?')
+    db.ref('feed').update({remy: count + 1})
   }
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    db.ref('feed/remy').on("value", snapshot => {
+      // should i change this?
+      const remy = snapshot.val()
+
+
+      setCount(remy)
+
+    }); 
+  });
+
 
   return (
     <div className="App">
       <button onClick={feedMeNow}>Feed Me! shNow!</button>
+      {count > 0 && <p>currently waiting to feed remy {count} number of times</p>}
     </div>
   );
 }
