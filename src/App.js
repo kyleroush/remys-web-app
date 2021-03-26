fb63a65f45c916773a6a68e46a91983e938bee59
kr043029 <kyle.roush@cerner.com>
2021-03-26 00:33:38 -0500

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { db } from './firestore';
 
 function App() {
+  const feedMeNow = () => {
+    db.ref('feednow').push('please?')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={feedMeNow}>Feed Me! shNow!</button>
     </div>
   );
 }
